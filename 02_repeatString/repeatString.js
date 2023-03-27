67919a443aabad5560292150afb684161ab27794
yehoki <patsenn2000@gmail.com>
2023-03-27 16:54:02 +0100

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return 'ERROR';
+    }
+    let ans = '';
+    for (let i = 0; i < num; i++){
+        ans = ans.concat(string);
+    }
+    return ans;
 };
 
 // Do not edit below this line
