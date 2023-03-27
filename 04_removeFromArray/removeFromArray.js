744da4b75e93c088ee5b9c677841fbfc8b84a165
yehoki <patsenn2000@gmail.com>
2023-03-27 17:17:49 +0100

    Finish exercise 4 JS

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numberList, ...numbers) {
+    let filteredArray = numberList;
+    for (let i = 0; i < numbers.length; i++){
+        filteredArray = filteredArray.filter((number) => number !== numbers[i])
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
