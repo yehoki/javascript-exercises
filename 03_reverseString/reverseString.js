fbf5954153aa99d497880bd2374f8b5a75bfa481
yehoki <patsenn2000@gmail.com>
2023-03-27 17:02:05 +0100

    Finish exercise 3 - reversing a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversed = '';
+    for (let i = word.length - 1; i >= 0; i--){
+        reversed += word[i]
+    }
+    return reversed;
 };
+
+// In the solution, we see the use of string.split which separates it into a list using some delimiter
+// Array.reverse which reverses an array in place and returns the same array
+// Array.join using the "" delimiter, to join an array into a string, by concatenating all of the elements in an array.
 
 // Do not edit below this line
 module.exports = reverseString;
