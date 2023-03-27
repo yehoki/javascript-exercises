cfd703de9ea55df3e5c35730ec1194c7580b4c7e
yehoki <patsenn2000@gmail.com>
2023-03-27 17:26:51 +0100

    Finish exercise 5 JS

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (typeof(first) !== 'number' || typeof(second) !== 'number' || Math.min(first, second) < 0){
+        return 'ERROR';
+    }
+    let answer = 0;
+    let lower = Math.min(first, second);
+    let higher = Math.max(first,second);
+    for (let i = lower; i <= higher; i++){
+        answer += i;
+    }
+    return answer;
 };
 
 // Do not edit below this line
