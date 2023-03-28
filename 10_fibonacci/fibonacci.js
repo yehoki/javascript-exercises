5e80474c127942f75cc1c4658c6a32cc2aaf858e
yehoki <patsenn2000@gmail.com>
2023-03-28 14:45:55 +0100

    Finish Fibonacci ex

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (parseInt(number) < 1){
+        return "OOPS";
+    }
+    if (parseInt(number) === 1){
+        return 1;
+    }
+    else if (parseInt(number) === 2){
+        return 1;
+    }
+    else {
+        return fibonacci(parseInt(number) - 1) + fibonacci(parseInt(number) - 2);
+    }
 };
 
 // Do not edit below this line
