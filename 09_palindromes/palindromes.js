dfd33c04dec46e6ee283125cb85938b2219132cd
yehoki <patsenn2000@gmail.com>
2023-03-28 14:40:04 +0100

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (entry) {
+    let newEntry = entry.replace(/[^a-z0-9]/gi, '');
+    return newEntry.toLowerCase().split("").reverse().join("") === newEntry.toLowerCase();
 };
 
 // Do not edit below this line
