c43c875a7943bbeb418354cb9942e34800f1ae0e
yehoki <patsenn2000@gmail.com>
2023-03-28 14:13:25 +0100

    Finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0){
+    return 0;
+  }
+  return array.reduce((total, digit) => {
+    return total + digit;
+  })
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0){
+    return 0;
+  }
+  return array.reduce((total, digit) => {
+    return total * digit;
+  })
 };
 
-const power = function() {
-	
+const power = function(digit, exponent) {
+  if (exponent === 0){
+    return 1;
+  } else if (exponent === 1){
+    return digit;
+  } else {
+    return digit * power(digit, exponent - 1);
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0){
+    return 1;
+  }
+  else if (number === 1){
+    return 1;
+  }
+  else {
+    return number * factorial(number - 1);
+  }
 };
 
 // Do not edit below this line
